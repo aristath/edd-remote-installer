cb730d330537730d3af49b8fe888e56a46a82d2b
Aristeides Stathopoulos <aristath@gmail.com>
2014-10-05 17:10:02 +0300

    add headers & include the Deploy client class

---
+++ client-plugin/client.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+Plugin Name: Easy Digital Downloads - Deployer (client demo plugin)
+Plugin URL: http://wpmu.io
+Description: Allows remote installation of WordPress plugins and themes
+Version: 1.0-dev1
+Author: Aristeides Stathopoulos
+Author URI: http://aristeides.com
+*/
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( 'EDD_Deploy_Client' ) ) {
+	include( dirname( __FILE__ ) . '/includes/class-EDD_Deploy_Client.php' );
+}

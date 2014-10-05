71d733ca34543490d8a430fc12e7e934c84a0b89
Aristeides Stathopoulos <aristath@gmail.com>
2014-10-05 20:49:59 +0300

    added an admin page to the demo client
    Sample markup… not that pretty but works. will be improved in the
    future.

--- client-plugin/client.php
+++ client-plugin/client.php
@@ -18,19 +18,25 @@ if ( ! class_exists( 'EDD_Deploy_Client' ) ) {
 	include( dirname( __FILE__ ) . '/includes/class-EDD_Deploy_Client.php' );
 }
 
-function prefix_define_downloads() {
+function prefix_define_downloads( $downloads ) {
 
-	$downloads = array(
-		'plugins' => array(
-			'Plugin 1',
-			'Plugin 2',
-			'Plugin 2',
-		),
-		'themes'  => array(
-			'Theme 1',
-			'Theme 2',
-			'Theme 3',
-		),
+	// Add our plugins
+	$downloads['Plugin 1'] = array(
+		'type'        => 'plugin',
+		'image'       => 'http://example.com/image.png',
+		'description' => 'This is the plugin description',
+	);
+	$downloads['Plugin 2'] = array(
+		'type'        => 'plugin',
+		'image'       => 'http://example.com/image.png',
+		'description' => 'This is the plugin description',
+	);
+
+	// Add our themes
+	$downloads['Theme 1'] = array(
+		'type'        => 'theme',
+		'image'       => 'http://example.com/image.png',
+		'description' => 'This is the theme description',
 	);
 
 	return $downloads;
@@ -38,4 +44,64 @@ function prefix_define_downloads() {
 }
 add_filter( 'prefix_edd_deployer_downloads', 'prefix_define_downloads' );
 
-function 
+
+class prefix_Admin_Page {
+
+	function __construct() {
+
+		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+		add_action( 'prefix_deployer_admin_page_content', 'prefix_deployer_admin_page_content' );
+
+	}
+
+	function admin_menu () {
+
+		add_options_page( 'Deployer Demo', 'Deployer Demo', 'install_plugins', 'deployer-demo', array( $this, 'settings_page' ) );
+	}
+
+	function  settings_page () {
+
+		$deployer = new EDD_Deploy_Client( 'http://example.com' );
+
+		$downloads = apply_filters( 'prefix_edd_deployer_downloads', array() );
+		?>
+
+		<div class="wrap">
+
+			<h2><?php _e( 'Deployer Demo', 'prefix' ); ?></h2>
+
+			<?php foreach ( $downloads as $download => $value ) { ?>
+
+				<div id="<?php echo sanitize_title( $download ); ?>" class="deploy-item postbox <?php echo $value['type']; ?>">
+					<h3 class="hndle"><span><?php echo $download; ?></span></h3>
+					<div class="inside">
+						<div class="main">
+
+							<?php if ( '' != $value['image'] ) : ?>
+								<img class="deployer-item-image" src="<?php echo $value['image']; ?>">
+							<?php endif; ?>
+
+							<?php if ( '' != $value['description'] ) : ?>
+								<p class="deployer-item-description"><?php echo $value['description']; ?></p>
+							<?php endif; ?>
+
+							<p class="deployer-actions">
+								<a class="button button-primary" href="<?php echo $deployer->install_url( $value['type'], $download ); ?>">
+									<?php _e( 'install', 'prefix' ); ?>
+								</a>
+							</p>
+						</div>
+					</div>
+				</div>
+
+			<?php } ?>
+
+		</div>
+
+
+		<?php
+
+	}
+
+}
+new prefix_Admin_Page;

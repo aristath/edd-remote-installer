1cb256b27cf706bab1a0a7f1491668f6e03521a1
Aristeides Stathopoulos <aristath@gmail.com>
2014-10-24 21:24:33 +0300

    implement new client admin page

--- client-plugin/includes/class-EDD_Deploy_Client.php
+++ client-plugin/includes/class-EDD_Deploy_Client.php
@@ -16,6 +16,16 @@ class EDD_Deploy_Client {
 //		add_action( 'wp_ajax_edd_deployer_check_download', array( $this, 'check_download') );
 //		add_action( 'wp_ajax_edd_deployer_deploy', array( $this, 'deploy') );
 
+		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+		add_action( 'prefix_deployer_admin_page_content', 'prefix_deployer_admin_page_content' );
+
+		add_thickbox();
+
+	}
+
+	function admin_menu () {
+
+		add_options_page( 'Deployer Demo', 'Deployer Demo', 'install_plugins', 'deployer-demo', array( $this, 'settings_page' ) );
 	}
 
 	public function register_scripts() {
@@ -31,6 +41,21 @@ class EDD_Deploy_Client {
 			return;
 			// TODO: Error message
 		}
+
+	}
+
+	function get_downloads() {
+
+		$api_params = array( 'edd_action' => 'get_downloads' );
+		$request    = wp_remote_post( $this->api_url . '/?edd_action=get_downloads' );
+
+		if ( is_wp_error( $request ) ) {
+			return;
+		}
+
+		$request = json_decode( wp_remote_retrieve_body( $request ), true );
+
+		return $request;
 
 	}
 
@@ -309,5 +334,63 @@ class EDD_Deploy_Client {
 			}
 		}
 		return $return;
+	}
+
+	function settings_page() {
+
+		echo '<style>.deploy-item { width: 32%; margin-right: 1%; float: left; min-width: 250px; } .deploy-item h3.hndle { padding: 0 1em 1em 1em; } .deploy-item img { width: 100%; height: auto; }</style>';
+
+		echo '<div class="wrap">';
+		echo '<h2>' . __( 'Deployer Demo', 'prefix' ) . '</h2>';
+
+		$downloads = $this->get_downloads();
+
+		$i = 0;
+
+		foreach ( $downloads as $download ) {
+
+			$data_free   = (int) $download['free'];
+			$disabled    = $this->is_plugin_installed( $download['title'] ) ? ' disabled="disabled" ' : '';
+			$button_text = $this->is_plugin_installed( $download['title'] ) ? __( 'Installed' ) : __( 'Install' );
+
+			$i = $i == 3 ? 0 : $i;
+			if ( $i == 0 ) {
+				echo '<div style="clear:both; display: block; float: none;"></div>';
+			}
+
+			echo '<div id="' . sanitize_title( $download['title'] ) . '" class="deploy-item postbox">';
+				echo '<h3 class="hndle"><span>' . $download['title'] . '</span></h3>';
+				echo '<div class="inside">';
+					echo '<div class="main">';
+
+						if ( '' != $download['thumbnail'] ) {
+							echo '<img class="deployer-item-image" src="' . $download['thumbnail'][0] . '">';
+						}
+
+						if ( '' != $download['description'] ) {
+							echo '<p class="deployer-item-description">' . $download['description'] . '</p>';
+						}
+
+						echo '<p class="deployer-actions">';
+							echo '<span class="spinner"></span>';
+							echo '<button class="button button-primary" data-free="' . $data_free . '"' . $disabled . 'data-deploy="' . $download['title'] . '">' . $button_text . '</button>';
+						echo '</p>';
+					echo '</div>';
+				echo '</div>';
+			echo '</div>';
+
+			$i++;
+		}
+
+		echo '<div id="edd_deployer_license_thickbox" style="display:none;">';
+		echo '<h3>' . __( 'Enter your license') . '</h3>';
+		echo '<form action="" method="post" id="edd_deployer_license_form">';
+		echo '<input style="width: 100%" type="text" id="edd_deployer_license"/>';
+		echo '<button style="margin-top: 10px" type="submit" class="button button-primary">' . __( 'Submit' ) . '</button>';
+		echo '</form>';
+		echo '</div>';
+
+		echo '</div>';
+
 	}
 }
